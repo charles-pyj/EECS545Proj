c06809dede3e66987ce89f0b76ca9ba1e2d80ca6
charlespyj <charles20021107@163.com>
2024-03-05 15:12:12 -0800

    Added/Added training scripts for gpt and t5

---
+++ scripts/train/train_spatial_flan_t5_model.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES="2,3,4" accelerate launch \
+    --num_processes=3 \
+    accelerate_spatial_flan_t5_model.py \
+    --wandb \
+    --run_name model_training_6_batch_flan_t5_large \
+    --model_name_or_path google/flan-t5-large \
+    --train_file /shared/3/projects/spatial-understanding/datasets/spatial_understanding_data_jsonl_full/train_spatial_understanding.csv \
+    --validation_file /shared/3/projects/spatial-understanding/datasets/spatial_understanding_data_jsonl_full/dev_spatial_understanding.csv \
+    --text_column question \
+    --summary_column answer \
+    --source_prefix "generate: " \
+    --num_beams 4 \
+    --output_dir /shared/3/projects/spatial-understanding/checkpoints_1109 \
+    --checkpointing_steps=400 \
---
+++ scripts/train/train_spatial_opt_model.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES="0,1" accelerate launch \
+    --num_processes=2 \
+    accelerate_spatial_causal_model.py \
+    --model_name_or_path "facebook/opt-350m" \
+    --train_file /shared/3/projects/spatial-understanding/sushrita-run-scripts/formatted_directions_train_detailed.csv \
+    --validation_file /shared/3/projects/spatial-understanding/sushrita-run-scripts/formatted_directions_dev_detailed.csv \
+    --text_column question \
+    --summary_column answer \
+    --output_dir /shared/3/projects/spatial-understanding/osrm-opt-out \
+    --overwrite_output_dir \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --do_train \
+    --do_eval
+
+
+    # --main_process_port=0 \
+    # --train_file /shared/3/projects/spatial-understanding/datasets/spatial_understanding_data_jsonl/train_spatial_understanding.csv \
+    # --validation_file /shared/3/projects/spatial-understanding/datasets/spatial_understanding_data_jsonl/dev_spatial_understanding.csv \
