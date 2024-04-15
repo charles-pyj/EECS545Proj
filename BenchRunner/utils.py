d40e75469c3ca4d5fd6104b2463cdcf819a36cc0
Sx Lau <3211220990@qq,com>
2024-04-14 23:05:58 -0400

    Update auto parser

--- BenchRunner/utils.py
+++ BenchRunner/utils.py
@@ -4,7 +4,7 @@ import re
 def extract_answer(input_string):
     """An automated parser to extract the answer from llm"""
     # Define the pattern to match the expression 'the answer is ...'
-    pattern = r'the answer is (.+?)(?=\.$|\.$)'
+    pattern = r'the answer is (.*?)(?=\.)'
 
     # Search for the pattern in the input string
     matches = re.findall(pattern, input_string, re.IGNORECASE)
